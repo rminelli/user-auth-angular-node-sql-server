49e4d2333730811ba52cd901e96ba55ac9a2cd48
Roberto Minelli <rminelli@gmail.com>
2019-07-25 12:04:53 -0300

    remove console.log

--- auth_api/index.js
+++ auth_api/index.js
@@ -2,18 +2,20 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const status = require('./status/status.js')
+const login_service = require('./login-service/login-service.js')
 
 app.use(bodyParser.json({ limit: '100mb' }))
 app.use(bodyParser.urlencoded({ extended: true, limit: '100mb' }))
 
 app.use(function (req, res, next) {   //Midleware
-    res.header("Access-Controle-Allow-Origin", "*")
-    res.header("Access-Controle-Allow-Headers", "Origin, X-Requested-With-Content-Type, Accept")
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     next()
 })
 
 module.exports = app
 app.get('/',status.go)
+app.post('/do-login',login_service.getLoginData)
 
 let server = app.listen(5510, function () {
     let host = server.address().address
