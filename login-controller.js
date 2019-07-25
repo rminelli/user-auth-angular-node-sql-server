14f791bb017b76611de81c16af0c1014c0fde583
Roberto Minelli <rminelli@gmail.com>
2019-07-25 14:46:06 -0300

    return responde data to $scope

--- login-controller.js
+++ login-controller.js
@@ -3,8 +3,11 @@ _authModule.controller('loginController', function ($scope, $http) {
     $scope.doLogin = async function (userData) {
         $scope.loading = true
         $http.post('http://localhost:5510/do-login', userData).then(function (response) { // Promisse
+            $scope.authResponse = response.data
+            $scope.loading = false
             console.log(response.data)
         }).catch(function (error) {
+            $scope.loading = false
             console.log(error)
         })
     }
