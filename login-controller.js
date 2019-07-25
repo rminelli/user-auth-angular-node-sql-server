5a063b551275e8658ddd93b6773e1c0a867b5a71
Roberto Minelli <rminelli@gmail.com>
2019-07-25 14:19:51 -0300

    change url for auth-service api

--- login-controller.js
+++ login-controller.js
@@ -2,7 +2,7 @@ _authModule.controller('loginController', function ($scope, $http) {
 
     $scope.doLogin = async function (userData) {
         $scope.loading = true
-        $http.post('http://127.0.0.1:5510/do-login', userData).then(function (response) { // Promisse
+        $http.post('http://localhost:5510/do-login', userData).then(function (response) { // Promisse
             console.log(response.data)
         }).catch(function (error) {
             console.log(error)
