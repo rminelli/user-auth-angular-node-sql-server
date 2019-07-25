8848ad9b34e989667408a63a8398dd5bd7678763
Roberto Minelli <rminelli@gmail.com>
2019-07-25 12:03:45 -0300

    Fix of user and password variables

--- auth_api/login-service/login-service.js
+++ auth_api/login-service/login-service.js
@@ -1,7 +1,7 @@
 exports.getLoginData = function (req, res) {
 
-    let _userName = req.body.userData._userName
-    let _userPassword = req.body.userData._userPassword
+    let _userName = req.body.userName
+    let _userPassword = req.body.userName
 
     console.log(_userName, _userPassword)
 
@@ -17,106 +17,106 @@ exports.getLoginData = function (req, res) {
         }
     }
 
-    // sql.connect(sqlConfig).then(function (pool) {
-    //     console.log("==== DATABASE CONNECTED =====");
+    sql.connect(sqlConfig).then(function (pool) {
+        console.log("==== DATABASE CONNECTED =====");
 
 
-    //     let query = "EXEC UserAuthentication '" + _userName + "'";
-    //     console.log("query  --->", query);
-    //     return pool.request().query(query).then(function (result) {
-    //         console.log("*** DETALHES RETORNADOS COM SUCESSO *** ");
+        let query = "EXEC UserAuthentication '" + _userName + "'";
+        console.log("query  --->", query);
+        return pool.request().query(query).then(function (result) {
+            console.log("*** DETALHES RETORNADOS COM SUCESSO *** ");
 
-    //         let _returnSql = result.recordset[0].RETURN;
+            let _returnSql = result.recordset[0].RETURN;
 
-    //         if (_returnSql === 1) {
-    //             sql.close();
-    //             return res.json(
-    //                 {
-    //                     "status": false,
-    //                     "msg": "Usuário inválido",
-    //                     "cod": 1
-    //                 }
-    //             )
-    //         }
-    //         else if (_returnSql === 2) {
-    //             sql.close();
-    //             return res.json(
-    //                 {
-    //                     "status": false,
-    //                     "msg": "Senha não cadastrada",
-    //                     "cod": 2
-    //                 }
-    //             )
-    //         }
-    //         else if (_returnSql === 22) {
-    //             sql.close();
-    //             return res.json(
-    //                 {
-    //                     "status": true,
-    //                     "msg": "Senha cadastrada com sucesso",
-    //                     "cod": 22
-    //                 }
-    //             )
-    //         }
-    //         else {
-    //             // Autenticação usuário - Descripitografando senha do banco
-    //             let _userpasswordDecrypted = CryptoJS.AES.decrypt(_returnSql.toString(), secretKey);
-    //             let _userpasswordString = _userpasswordDecrypted.toString(CryptoJS.enc.Utf8);
-    //             if (_userpasswordString == _userpasswordformString) {
-    //                 console.log("Autenticado");
-    //                 sql.close();
-    //                 delete result.recordset[0].RETURN;
-    //                 res.json(
-    //                     {
-    //                         "status": true,
-    //                         "msg": "Usuário Válido",
-    //                         "userData": result.recordset[0],
-    //                         "cod": 3
-    //                     }
-    //                 )
+            if (_returnSql === 1) {
+                sql.close();
+                return res.json(
+                    {
+                        "status": false,
+                        "msg": "Usuário inválido",
+                        "cod": 1
+                    }
+                )
+            }
+            else if (_returnSql === 2) {
+                sql.close();
+                return res.json(
+                    {
+                        "status": false,
+                        "msg": "Senha não cadastrada",
+                        "cod": 2
+                    }
+                )
+            }
+            else if (_returnSql === 22) {
+                sql.close();
+                return res.json(
+                    {
+                        "status": true,
+                        "msg": "Senha cadastrada com sucesso",
+                        "cod": 22
+                    }
+                )
+            }
+            else {
+                // Autenticação usuário - Descripitografando senha do banco
+                let _userpasswordDecrypted = CryptoJS.AES.decrypt(_returnSql.toString(), secretKey);
+                let _userpasswordString = _userpasswordDecrypted.toString(CryptoJS.enc.Utf8);
+                if (_userpasswordString == _userpasswordformString) {
+                    console.log("Autenticado");
+                    sql.close();
+                    delete result.recordset[0].RETURN;
+                    res.json(
+                        {
+                            "status": true,
+                            "msg": "Usuário Válido",
+                            "userData": result.recordset[0],
+                            "cod": 3
+                        }
+                    )
 
-    //             }
-    //             else {
-    //                 console.log("Não autenticado");
-    //                 sql.close();
-    //                 res.json(
-    //                     {
-    //                         "status": false,
-    //                         "msg": "Senha incorreta",
-    //                         "cod": 4
-    //                     }
-    //                 )
+                }
+                else {
+                    console.log("Não autenticado");
+                    sql.close();
+                    res.json(
+                        {
+                            "status": false,
+                            "msg": "Senha incorreta",
+                            "cod": 4
+                        }
+                    )
 
-    //             }
-    //         }
-
-
-    //     }).catch(function (err) {
-    //         console.log("ERROR SQL", err);
-    //         sql.close();
-    //         res.json(
-    //             {
-    //                 "status": false,
-    //                 "msg": "ERROR SQL"
-    //             }
-    //         )
-    //     });
-
-    // }).catch(function (errsql) {
-    //     console.log("ERRO SQL PROTHEUS", errsql);
-    //     sql.close();
-    //     res.json(
-    //         {
-    //             "status": false,
-    //             "msg": "ERRO SQL PROTHEUS"
-    //         }
-    //     )
-    // })
+                }
+            }
 
 
+        }).catch(function (err) {
+            console.log("ERROR SQL", err);
+            sql.close();
+            res.json(
+                {
+                    "status": false,
+                    "msg": "ERROR SQL"
+                }
+            )
+        });
+
+    }).catch(function (errsql) {
+        console.log("ERRO SQL PROTHEUS", errsql);
+        sql.close();
+        res.json(
+            {
+                "status": false,
+                "msg": "ERRO SQL PROTHEUS"
+            }
+        )
+    })
 
 
 
-    console.log(req.body)
+
+
+   
 
 }
