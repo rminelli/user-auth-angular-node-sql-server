0ee1846a66eae9e0ddfce4c2d0229a544f6db66f
Roberto Minelli <rminelli@gmail.com>
2019-07-25 11:02:38 -0300

    http responde implementation

--- login-controller.js
+++ login-controller.js
@@ -1,10 +1,16 @@
-_authModule.controller('loginController', function ($scope) {
+_authModule.controller('loginController', function ($scope, $http) {
 
-    $scope.doLogin = async function(userData){
+    $scope.doLogin = async function (userData) {
         $scope.loading = true
-        console.log(userData)
+        let _userName = userData.userName
+        let _userPassword = userData.userPassword
+        console.log(`User name: ${_userName} - User password: ${_userPassword}`)
+
+        $http.post('http://localhost:5500', userData).then(function (response) {
+            console.log(response)
+        }).catch(function (error) {
+            console.log(error)
+        })
     }
 
-    
-    
 });
