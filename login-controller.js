f987c76e56f36b56560daad26d1efb816b043266
Roberto Minelli <rminelli@gmail.com>
2019-07-25 12:04:15 -0300

    remove console.log

--- login-controller.js
+++ login-controller.js
@@ -2,10 +2,6 @@ _authModule.controller('loginController', function ($scope, $http) {
 
     $scope.doLogin = async function (userData) {
         $scope.loading = true
-        let _userName = userData.userName
-        let _userPassword = userData.userPassword
-        console.log(`User name: ${_userName} - User password: ${_userPassword}`)
-
         $http.post('http://127.0.0.1:5510/do-login', userData).then(function (response) { // Promisse
             console.log(response.data)
         }).catch(function (error) {
