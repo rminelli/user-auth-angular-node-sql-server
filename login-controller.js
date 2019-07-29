bc071f92ca1f843d14f07edb751b394b50ef0d50
Roberto Minelli <rminelli@gmail.com>
2019-07-29 14:25:46 -0300

    Data return handling, sent by scope

--- login-controller.js
+++ login-controller.js
@@ -2,7 +2,7 @@ _authModule.controller('loginController', function ($scope, $http) {
 
     $scope.doLogin = async function (userData) {
         $scope.loading = true
-        $http.post('http://localhost:5510/do-login', userData).then(function (response) { // Promisse
+        $http.post('http://localhost:5510/do-login', userData).then(function (response) { // Promisse do-login
             $scope.authResponse = response.data
             $scope.loading = false
             console.log(response.data)
